d929300bbb155af6efaf9dbaafeccd9370c85683
adosoyola <75389246@continental.edu.pe>
2025-11-06 00:21:13 -0500

    ultimo cambio panel

--- ECommerceGrupo01/obj/Debug/net8.0/ECommerce.AssemblyInfo.cs
+++ ECommerceGrupo01/obj/Debug/net8.0/ECommerce.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ECommerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d32850395e84e19fe06e7df6e8703132f2f7948")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+533981f3f4b16b7ab823d9ab52024ee76416429f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ECommerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ECommerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
