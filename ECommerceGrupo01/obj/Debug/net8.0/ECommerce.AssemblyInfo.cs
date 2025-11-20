ce47b8d158292c4ec8786e1b4b10233bd0228ab9
adosoyola <75389246@continental.edu.pe>
2025-11-19 20:15:27 -0500

    Cambios 19/11

--- ECommerceGrupo01/obj/Debug/net8.0/ECommerce.AssemblyInfo.cs
+++ ECommerceGrupo01/obj/Debug/net8.0/ECommerce.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ECommerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+500cb632e2ccf7ccebbef83af6fb73f2bf93f2e3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0296e59c8ce1dd862eafb15036213fc37e7b0ce4")]
 [assembly: System.Reflection.AssemblyProductAttribute("ECommerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ECommerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
